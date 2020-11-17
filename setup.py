0f8e2cb77b24eaa4e64bd69c50d1f39db9bcd633
Cariad Eccleston <cariad@cariad.me>
2020-11-17 12:40:02 +0000

    Out of beta (#6)
    * Increase test coverage failure threshold to 95%
    
    * Remove unused requirements.txt
    
    * Set correct development classifier

--- setup.py
+++ setup.py
@@ -9,25 +9,36 @@ readme_path = Path(__file__).parent.joinpath("README.md")
 with open(readme_path, encoding="utf-8") as f:
     long_description = f.read()
 
+classifiers = [
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Topic :: Internet",
+    "Topic :: Security",
+    "Topic :: Utilities",
+]
+
+version = get_version()
+
+if "a" in version:
+    classifiers.append("Development Status :: 3 - Alpha")
+elif "b" in version:
+    classifiers.append("Development Status :: 4 - Beta")
+else:
+    classifiers.append("Development Status :: 5 - Production/Stable")
+
+classifiers.sort()
+
 setup(
     author="Cariad Eccleston",
     author_email="cariad@cariad.me",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet",
-        "Topic :: Security",
-        "Topic :: Software Development :: Version Control :: Git",
-        "Topic :: Utilities",
-    ],
+    classifiers=classifiers,
     description="AWS CodeArtifact authorisation token generator for CLI tools",
     entry_points={
         "console_scripts": [
@@ -54,5 +65,5 @@ setup(
     packages=find_packages(),
     python_requires=">=3.8",
     url="https://github.com/cariad/corf",
-    version=get_version(),
+    version=version,
 )
